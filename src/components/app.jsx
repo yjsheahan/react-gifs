da0220a6a486cb0d64f1624648772e5cae265a86
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-06-22 14:35:04 +0100

    gif app almost done, need to add onClick function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+const GIPHY_API_KEY = 'c4GpuGoBsD170hRFFDGrVS1ZZlMGnWTo';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "3jVT4U5bilspG"
+    };
+    // this.search("homer");
+  }
+
+  search = (query) => {
+    giphy({ GIPHY_API_KEY }).search({
+      q: query,
+      rating: 'g'
+    }, (err, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        <div className="left-scene"></div>
-        <div className="right-scene"></div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-import App from './components/app.jsx';
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import App from './components/app';
 
 const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App />, root);
-}
+ReactDOM.render(<App />, root);
